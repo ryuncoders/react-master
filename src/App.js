2cec808e377fc6c91a364976254d092bc802a0eb
ryuncoders <seungjy10@gmail.com>
2024-03-14 14:03:46 +0900

    chore(style): Themes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 
-const Father = styled.div`
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+const Wrapper = styled.div`
+  margin: 0;
+  padding: 0;
   display: flex;
-`;
-
-const Box = styled.div`
-  height: 100px;
-  width: 100px;
-  background-color: ${(props) => props.bgColor};
-`;
-
-const Circle = styled(Box)`
-  border-radius: 50%;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
+  // Takes props from the parent component
 `;
 
 function App() {
   return (
-    <Father as="header">
-      <Box bgColor="teal" />
-      <Circle bgColor="whitesmoke" />
-    </Father>
+    <Wrapper>
+      <Title>Hello world</Title>
+    </Wrapper>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
+
+const darkTheme = {
+  textColor: "whitesmoke",
+  backgroundColor: "black",
+};
+
+const lightTheme = {
+  textColor: "black",
+  backgroundColor: "whitesmoke",
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
