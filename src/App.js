81fc51a40942b775bd4c0f806d5aa1ade18c57ca
ryuncoders <seungjy10@gmail.com>
2024-03-14 13:09:20 +0900

    fix(style): using styled-components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+const BoxOne = styled.div`
+  background-color: teal;
+  width: 100px;
+  height: 100px;
+`;
+
+const BoxTwo = styled.div`
+  background-color: tomato;
+  width: 100px;
+  height: 100px;
+`;
+
 function App() {
   return (
-    <div style={{ display: "flex " }}>
-      <div
-        style={{ backgroundColor: "teal", width: "100px", height: "100px" }}
-      ></div>
-      <div
-        style={{ backgroundColor: "tomato", width: "100px", height: "100px" }}
-      ></div>
-    </div>
+    <Father>
+      <BoxOne></BoxOne>
+      <BoxTwo></BoxTwo>
+    </Father>
   );
 }
 
