e0e9ffbaf6e8c634c1445da61fc19415685d0919
ryuncoders <seungjy10@gmail.com>
2024-03-14 13:43:28 +0900

    chore(style): Extending

--- src/App.js
+++ src/App.js
@@ -2,44 +2,23 @@ import styled, { keyframes } from "styled-components";
 
 const Father = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
 `;
-const rotationAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-  }
-  50% {
-    transform: rotate(180deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-`;
-const Emoji = styled.span`
-  font-size: 35px;
-`;
+
 const Box = styled.div`
-  width: 100px;
   height: 100px;
-  background-color: tomato;
-  animation: ${rotationAnimation} 1s linear infinite;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  ${Emoji}:hover {
-    font-size: 50px;
-  }
+  width: 100px;
+  background-color: ${(props) => props.bgColor};
+`;
+
+const Circle = styled(Box)`
+  border-radius: 50%;
 `;
 
 function App() {
   return (
     <Father as="header">
-      <Box>
-        <Emoji>🤸‍♀️</Emoji>
-      </Box>
-      <Emoji>💙</Emoji>
+      <Box bgColor="teal" />
+      <Circle bgColor="whitesmoke" />
     </Father>
   );
 }
