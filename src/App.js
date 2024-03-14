e7b7a54a2e54d0ca98bcf541df6861cc2b5c6c50
ryuncoders <seungjy10@gmail.com>
2024-03-14 13:30:03 +0900

    chore(style): animations and pseudo selectors(like SASS)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const Father = styled.div`
   display: flex;
 `;
-
-const Input = styled.input.attrs({ required: true })`
-  background-color: toamto;
+const rotationAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  50% {
+    transform: rotate(180deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+const Box = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: tomato;
+  animation: ${rotationAnimation} 1s linear infinite;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  span {
+    font-size: 35px;
+    &:hover {
+      font-size: 50px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
 `;
 
 function App() {
   return (
     <Father as="header">
-      <Input />
+      <Box>
+        <span>🤸‍♀️</span>
+      </Box>
     </Father>
   );
 }
