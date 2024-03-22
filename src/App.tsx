e0a55b4f733d03363f85a8ca740b35de92ac2ed2
ryuncoders <seungjy10@gmail.com>
2024-03-22 18:14:06 +0900

    feat(CRYPTO TRACKER): styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,31 @@
+import React from "react";
 import Router from "./Router";
+import { Reset } from "styled-reset";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Mynerve&display=swap');
+  a {
+    text-decoration: none;
+  }
+  body {
+    background-color: ${(props) => props.theme.bgColor};
+    color: ${(props) => props.theme.textColor};
+    font-family: "Mynerve", 'Source Sans Pro', sans-serif;
+    font-weight: 400;
+    font-style: normal;
+  }
+`;
 
 function App() {
-  return <Router />;
+  return (
+    <>
+      <Reset />
+      <GlobalStyle />
+      <Router />
+    </>
+  );
+  //React.Fragment
 }
 export default App;
