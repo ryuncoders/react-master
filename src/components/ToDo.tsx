04536990f25ff470254713cb2c2947396073ddbe
ryuncoders <seungjy10@gmail.com>
2024-04-30 14:27:35 +0900

    feat(ToDoList): Categories

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,31 @@
+import React from "react";
 import { IToDo } from "./atoms";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+    console.log("i want a see", name);
+  };
   return (
     <li>
       <span>{text}</span>
-      <button>Doing</button>
-      <button>To Do</button>
-      <button>Done</button>
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClick}>
+          TO DO
+        </button>
+      )}
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClick}>
+          DONE
+        </button>
+      )}
     </li>
   );
 }
