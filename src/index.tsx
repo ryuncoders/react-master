de53acf87483234645af5b314107f02d6f94fe71
ryuncoders <seungjy10@gmail.com>
2024-05-16 15:56:04 +0900

    Ref(framer-motion): setup

--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,7 @@ const GlobalStyle = createGlobalStyle`
   background-color: ${(props) => props.theme.bgColor};
   color: black;
   line-height: 1.2;
+  background:linear-gradient(135deg,#e09,#d0e);
 }
 `;
 
