482f5522d9029b1a7c37256e3c7178e329557fb6
ryuncoders <seungjy10@gmail.com>
2024-05-17 11:54:11 +0900

    Ref(framer-motion): Gestures, Drag

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { useRef } from "react";
 import styled from "styled-components";
 import { motion, Variants } from "framer-motion";
 
 const Wrapper = styled.div`
   height: 100vh;
-  width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -16,68 +15,41 @@ const Box = styled(motion.div)`
   background-color: white;
   border-radius: 35px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
 `;
 
-const Circle = styled(motion.div)`
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  width: 70px;
-  height: 70px;
-  border-radius: 50%;
-  place-self: center;
-  background-color: whitesmoke;
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* overflow: hidden; */
 `;
-
-// 1)
-const myAnimat = {
-  start: { scale: 0 },
-  end: {
-    scale: 1,
-    rotateZ: 360,
-    transition: { taype: "spring", delay: 0.25, bounce: 100 },
-  },
-};
-
-// 2)
 
 const box: Variants = {
-  start: {
-    opacity: 0,
-    scale: 0.5,
-  },
-  end: {
-    opacity: 1,
-    scale: 1,
-    transition: {
-      type: "spring",
-      duration: 0.5,
-      bounce: 0.5,
-      delayChildren: 0.5,
-      staggerChildren: 0.2,
-    },
-  },
-};
-const circle: Variants = {
-  start: {
-    opacity: 0,
-    y: 10,
-  },
-  end: {
-    opacity: 1,
-    y: 0,
-  },
+  hover: { scale: 1, rotateZ: 90 },
+  tap: { scale: 1, borderRadius: "50%" },
+  drag: { backgroundColor: "rgb(34, 47, 62)" },
 };
 
 function App() {
+  const boxConstratints = useRef(null);
   return (
     <Wrapper>
-      <Box variants={box} initial="start" animate="end">
-        <Circle variants={circle} initial="start" animate="end"></Circle>
-        <Circle variants={circle} initial="start" animate="end"></Circle>
-        <Circle variants={circle} initial="start" animate="end"></Circle>
-        <Circle variants={circle} initial="start" animate="end"></Circle>
-      </Box>
+      <BiggerBox ref={boxConstratints}>
+        <Box
+          variants={box}
+          drag
+          dragConstraints={boxConstratints}
+          // dragSnapToOrigin
+          dragElastic={1}
+          whileHover="hover"
+          whileTap="tap"
+          whileDrag="drag"
+        ></Box>
+      </BiggerBox>
     </Wrapper>
   );
 }
